04e73dea0066d38dfcf879a19b3a66391f16a4e7
ktunprasert <ktunprasert@outlook.com>
2022-12-02 09:42:12 +0000

    day1: added

---
+++ src/Day1/Solver.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Day1;
+
+use SplMaxHeap;
+
+class Solver
+{
+    public function solvePartOne(string $input): int
+    {
+        $previousElf = 0;
+        $elf = 0;
+
+        foreach (explode("\n", $input) as $line) {
+            if ($line) {
+                $elf += (int) $line;
+            } else {
+                if ($elf > $previousElf) {
+                    $previousElf = $elf;
+                }
+
+                $elf = 0;
+            }
+        }
+
+        return max($previousElf, $elf);
+    }
+
+    public function solvePartTwo(string $input): int
+    {
+        $previousElves = [];
+        $elf = 0;
+
+        foreach (explode("\n", $input) as $line) {
+            if ($line) {
+                $elf += (int) $line;
+            } else {
+                if (count($previousElves) < 3) {
+                    $previousElves[] = $elf;
+                } else {
+                    sort($previousElves);
+                    foreach ($previousElves as $i => $pElf) {
+                        if ($elf > $pElf) {
+                            $previousElves[$i] = $elf;
+                            break;
+                        }
+                    }
+                }
+                $elf = 0;
+            }
+        }
+
+        sort($previousElves);
+        foreach ($previousElves as $i => $pElf) {
+            if ($elf > $pElf) {
+                $previousElves[$i] = $elf;
+                break;
+            }
+        }
+
+        return array_sum($previousElves);
+    }
+
+    public function solveMaxHeap(string $input, int $count = 1): int
+    {
+        $maxHeap = new SplMaxHeap();
+        $elf = 0;
+        foreach (explode("\n", $input) as $line) {
+            if ($line) {
+                $elf += (int) $line;
+            } else {
+                $maxHeap->insert($elf);
+                $elf = 0;
+            }
+        }
+
+        $maxHeap->insert($elf);
+
+        $total = 0;
+        foreach (range(0, $count - 1) as $_) {
+            $total += $maxHeap->extract();
+        }
+
+        return $total;
+    }
+}
---
+++ tests/Unit/Day1/SolverTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit\Day1;
+
+use App\Day1\Solver;
+use PHPUnit\Framework\TestCase;
+use Lib\AdventClient;
+
+class SolverTest extends TestCase
+{
+    public function test_it_pass_example_test(): void
+    {
+        $input = <<<str
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+str;
+
+        $solver = new Solver();
+        $this->assertEquals(24000, $solver->solvePartOne($input));
+        $this->assertEquals(45000, $solver->solvePartTwo($input));
+
+        $this->assertEquals(24000, $solver->solveMaxHeap($input));
+        $this->assertEquals(45000, $solver->solveMaxHeap($input, 3));
+    }
+
+    public function test_it_submits_correct_answer(): void
+    {
+        // $this->markTestSkipped('Day 1 tests already submitted skipped to not spam the server');
+
+        $client = new AdventClient(2022, 1);
+        $inputRes = $client->getInput();
+        $solver = new Solver();
+
+        if ($inputRes->getStatusCode() === 200) {
+            $input = (string) $inputRes->getBody();
+
+            if (!$client->submittedPartOne()) {
+                $partOneRes = $client->submitPartOne($solver->solvePartOne($input));
+                $this->assertStringNotContainsStringIgnoringCase('not the right answer', (string) $partOneRes->getBody());
+            }
+
+            if (!$client->submittedPartTwo()) {
+                $partTwoRes = $client->submitPartTwo($solver->solvePartTwo($input));
+                $this->assertStringNotContainsStringIgnoringCase('not the right answer', (string) $partTwoRes->getBody());
+            }
+        }
+    }
+}
