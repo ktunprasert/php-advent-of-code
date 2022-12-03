343ff8adcba3dbc7855876728cbd32e301cef0af
ktunprasert <ktunprasert@outlook.com>
2022-12-03 22:20:24 +0000

    day3: added

---
+++ src/Day3/Solver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Day3;
+
+class Solver
+{
+    public function solvePartOne(string $input): int
+    {
+        $duped = [];
+        foreach (explode("\n", $input) as $line) {
+            if (!$line) continue;
+            $midLength = strlen($line) / 2;
+            $leftSack = [];
+
+            foreach (range(0, $midLength - 1) as $i) {
+                $leftSack[$line[$i]] = 1;
+            }
+
+            foreach (range($midLength, strlen($line) - 1) as $i) {
+                if (isset($leftSack[$line[$i]])) {
+                    $duped[] = $line[$i];
+                    break;
+                }
+            }
+        }
+
+        $sum = 0;
+        foreach ($duped as $char) {
+            $sum += $this->getPriority($char);
+        }
+
+        return $sum;
+    }
+
+    public function solvePartTwo(string $input): int
+    {
+        $sum = 0;
+
+        foreach (array_chunk(explode("\n", $input), 3) as $chunk) {
+            if (count($chunk) === 1) break;
+            $map = array_fill(0, 52, 0);
+            $round = 1;
+            foreach ($chunk as $line) {
+                foreach (str_split($line) as $c) {
+                    $idx = $this->getPriority($c) - 1;
+                    if ($map[$idx] === $round - 1) {
+                        $map[$idx] = $round;
+                    }
+                }
+                $round++;
+            }
+            $sum += array_search(3, $map) + 1;
+        }
+
+        return $sum;
+    }
+
+    private function getPriority(string $char): int
+    {
+        switch ($char) {
+            case ($char >= 'a' && $char <= 'z'):
+                return ord($char) - 96;
+            case ($char >= 'A' && $char <= 'Z'):
+                return ord($char) - 64 + 26;
+        };
+    }
+}
---
+++ tests/Unit/Day3/SolverTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit\Day3;
+
+use App\Day3\Solver;
+use PHPUnit\Framework\TestCase;
+use Lib\AdventClient;
+
+class SolverTest extends TestCase
+{
+    public function test_it_pass_example_test(): void
+    {
+        $input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+
+        $solver = new Solver();
+
+        $this->assertEquals(157, $solver->solvePartOne($input));
+        $this->assertEquals(70, $solver->solvePartTwo($input));
+    }
+
+    public function test_it_submits_correct_answer(): void
+    {
+        $this->markTestSkipped('Day 3 tests already submitted skipped to not spam the server');
+
+        $client = new AdventClient(2022, 3);
+        $inputRes = $client->getInput();
+        $solver = new Solver();
+
+        if ($inputRes->getStatusCode() === 200) {
+            $input = (string) $inputRes->getBody();
+
+            if (!$client->submittedPartOne()) {
+                $partOneRes = $client->submitPartOne($solver->solvePartOne($input));
+                $this->assertStringNotContainsStringIgnoringCase('not the right answer', (string) $partOneRes->getBody());
+            }
+
+            if (!$client->submittedPartTwo()) {
+                $partTwoRes = $client->submitPartTwo($solver->solvePartTwo($input));
+                $this->assertStringNotContainsStringIgnoringCase('not the right answer', (string) $partTwoRes->getBody());
+            }
+        }
+    }
+}
