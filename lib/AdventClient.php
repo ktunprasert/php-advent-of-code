444a377e4ced3f06fc840101853c355fe0cf2645
ktunprasert <ktunprasert@outlook.com>
2022-12-12 01:21:55 +0000

    adventclient: build submission status yearly only once

--- lib/AdventClient.php
+++ lib/AdventClient.php
@@ -7,16 +7,19 @@ use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Cookie\CookieJar;
 use GuzzleHttp\Cookie\CookieJarInterface;
 use GuzzleHttp\Psr7\Response;
+use Symfony\Component\DomCrawler\Crawler;
 
 class AdventClient
 {
     private ClientInterface $client;
+    private static array $submissionStatus = [];
 
     public function __construct(
         private int $year,
         private int $day,
     ) {
         $this->client = new Client(['cookies' => $this->makeCookies()]);
+        $this->buildCurrentYearSubmissionStatus();
     }
 
     public function getInput(): Response
@@ -24,16 +27,29 @@ class AdventClient
         return $this->client->request('GET', "https://adventofcode.com/$this->year/day/$this->day/input");
     }
 
+    public function getDateStars(): int
+    {
+        return static::$submissionStatus[$this->year][$this->day] ?? 0;
+    }
+
+    public function solved(): bool
+    {
+        return $this->getDateStars() == 2;
+    }
+
+    public function unsolved(): bool
+    {
+        return $this->getDateStars() == 0;
+    }
+
     public function submittedPartOne(): bool
     {
-        $res = $this->client->request('GET', 'https://adventofcode.com');
-        return str_contains((string)$res->getBody(), "calendar-day$this->day calendar-complete") || str_contains((string)$res->getBody(), "calendar-day$this->day calendar-verycomplete");
+        return $this->getDateStars() >= 1;
     }
 
     public function submittedPartTwo(): bool
     {
-        $res = $this->client->request('GET', 'https://adventofcode.com');
-        return str_contains((string)$res->getBody(), "calendar-day$this->day calendar-verycomplete");
+        return $this->getDateStars() == 2;
     }
 
     public function submitPartOne(mixed $answer): Response
@@ -54,6 +70,29 @@ class AdventClient
                 'answer' => $answer,
             ],
         ]);
+    }
+
+    private function buildCurrentYearSubmissionStatus(): void
+    {
+        if (isset(static::$submissionStatus[$this->year])) return;
+
+        $res = $this->client->request('GET', "https://adventofcode.com/$this->year");
+        $body = (string) $res->getBody();
+
+        $crawler = new Crawler($body);
+        $calendar = array_fill_keys(range(1, 25), 0);
+        foreach ($crawler->filter('.calendar > a')->extract(['class']) as $availableDays) {
+            [$dayText, $dayStatus] = explode(' ', $availableDays);
+            $day = abs(filter_var($dayText, FILTER_SANITIZE_NUMBER_INT));
+
+            $calendar[$day] = match ($dayStatus) {
+                'calendar-complete' => 1,
+                'calendar-verycomplete' => 2,
+                default => 0,
+            };
+        }
+
+        static::$submissionStatus[$this->year] = $calendar;
     }
 
     private function makeCookies(): CookieJarInterface
